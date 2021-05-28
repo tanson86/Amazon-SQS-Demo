cc928187aff5339a931831b2c9f29251bf63ca84
tanson@86 <tanson86@gmail.com>
2021-05-28 06:43:09 +0530

    first commit

---
+++ src/test/java/com/sqs/push/demo/sqspush/SqsPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqs.push.demo.sqspush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
